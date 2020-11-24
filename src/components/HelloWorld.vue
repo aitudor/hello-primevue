271af661f68f4ebfa1c30b92440cb4c975da3896
Andrei Tudor <andrei.tudor@theopenskyproject.com>
2020-11-24 17:18:37 +0200

    added datatable

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,16 +28,60 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
+   <div v-for="car of cars" v-bind:key="car.year">
+    <p> <span>{{ car.brand }}</span> <span>{{ car.year }}</span> <span>{{ car.vin }}</span> </p>
+  </div>
+
+  <Button label="Submit"></Button>
+
+<DataTable :value="cars">
+    <template #header>
+        <div>
+            <Button icon="pi pi-refresh" style="float: left"/>
+            List of Cars
+        </div>
+    </template>
+    <Column field="vin" header="Vin" ></Column>
+    <Column field="year" header="Year" ></Column>
+    <Column field="brand" header="Brand" ></Column>
+    <Column field="color" header="Color" ></Column>
+</DataTable>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from 'primevue/button';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';     //optional for column grouping
 
 export default defineComponent({
   name: 'HelloWorld',
   props: {
-    msg: String,
+    msg: String
   },
+  components: {
+    Button,
+    DataTable,
+    Column
+  },
+  data() {
+    const cars = [{"brand": "Volkswagen", "year": 2012, "color": "Orange", "vin": "dsad231ff"},
+        {"brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345"},
+        {"brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr"},
+        {"brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh"},
+        {"brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34"},
+        {"brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj"},
+        {"brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr"},
+        {"brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34"},
+        {"brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5"},
+        {"brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s"}];
+    return {
+      cars
+    };
+  }
 });
 </script>
 
